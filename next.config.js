13066eda1f171b01dd98470bf0b3a33a0358b8eb
Matt Avila <mexicanfewgitive@gmail.com>
2025-07-22 03:44:47 -0500

    next.config again

--- next.config.js
+++ next.config.js
@@ -1,19 +1,11 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// Replicate __dirname functionality for ES Module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your other Next.js config might be here
-  
-  // Add the webpack configuration below
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     config.resolve.alias['@'] = path.join(__dirname, 'src');
     return config;
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
